e78d3b476b0f223fbe4a44e5cbbd6283596dd068
Adam Ingram <kr0nai84@gmail.com>
2024-03-24 19:46:53 -0700

    MVP reached

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -60,3 +60,82 @@ expressRouter.post('/', (req, res) => {
             )
     }
 })
+
+expressRouter.put('/:id', (req, res) => {
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post",
+            error: err.message
+        })
+    } else {
+        console.log("You made it!")
+        postsModel.findById(req.params.id)
+            .then(post => {
+                if (!post) {
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist",
+                        error: err.message
+                    })
+                } else {
+                    return postsModel.update(req.params.id, req.body)
+                }
+            })
+            .then(data => {
+                if (data) {
+                    return postsModel.findById(req.params.id)
+                }
+            })
+            .then(post => {
+                if (post) {
+                    res.json(post);
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "The post information could not be modified",
+                    error: err.message
+                })
+            })
+    }
+})
+
+expressRouter.delete('/:id', async (req, res) => {
+    try {
+        const post = await postsModel.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+                error: err.message
+            })
+        } else {
+            await postsModel.remove(req.params.id)
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post could not be removed",
+            error: err.message
+        })
+    }
+})
+
+expressRouter.get('/:id/comments', async (req, res) => {
+    try {
+        const post = await postsModel.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+                error: err.message
+            })
+        } else {
+            const comments = await postsModel.findPostComments(req.params.id)
+            res.json(comments);
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The comments information could not be retrieved",
+            error: err.message
+        })
+    }
+})
